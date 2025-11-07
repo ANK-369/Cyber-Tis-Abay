3dfea4580e511159049bef558355106e9b18333d
ANK-369 <ucancmeank@gmail.com>
2025-11-07 06:50:58 +0300

    Add Car class with encapsulation example
    Implemented a Car class demonstrating encapsulation with private members and public methods for data access.

---
+++ car_encapsulation.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// Car class
+class Car {
+private:
+    string brand; // private member
+    int year;  // private member 
+
+public:
+    // Method to set data
+    void setData(string b, int y) {
+        brand = b;
+        year = y;
+    }
+
+    // Method to display data
+    void display() {
+        cout << "Car Brand: " << brand << endl;
+        cout << "Manufacturing Year: " << year << endl;
+    }
+};
+
+int main() {
+    Car car1;
+
+    car1.setData("Toyota", 2022);
+
+    // Displaying data
+    car1.display();
+
+    /* 
+     Trying to access private data directly:
+     car1.brand = "BMW";  --> ERROR: 'brand' is private within this context
+     cout << car1.brand;  --> ERROR: cannot access private member
+    */
+    cout << "\nIf you try to access 'brand' directly from main(), the compiler gives an error." << endl;
+    cout << "That’s encapsulation in action private data is hidden from direct access!" << endl;
+
+    return 0;
+}
